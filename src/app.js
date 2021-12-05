70f51b901f70358373c6d47c5cf41d995f75bac8
dongjaelee <milkbear0313@gmail.com>
2021-12-05 14:27:54 +0900

    Add cors permission

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const roomRouter = require("./routes/room");
 const userRouter = require("./routes/user");
 const app = express();
